c09766b9fc793d1cffe957b4cbc77cd0972be198
Julio <julio@estudio.com>
2026-04-06 09:21:24 -0600

    feat: implement remote theme discovery engine (v0.0.1-alpha.2)

--- PoshBuddy.ps1
+++ PoshBuddy.ps1
@@ -5,3 +5,7 @@ $PoshBuddy = @{
     Config = @{}
 }
 Write-Host "PoshBuddy Engine Initialized"
+function Get-RemoteThemes {
+    $url = "https://api.github.com/repos/JanDeDobbeleer/oh-my-posh/contents/themes"
+    return (Invoke-RestMethod -Uri $url | Where-Object { $_.name -like "*.omp.json" }).name
+}
